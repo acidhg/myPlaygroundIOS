450957ce67142373fe7d32cae62b979c110e2f77
HyungeunJo <hyungeun.jo@keukey.com>
2016-12-23 16:28:17 +0900

    first commit

---
+++ play1/Play1_1/Play1_1/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Play1_1
+//
+//  Created by JOHYUNGEUN on 2016. 12. 22..
+//  Copyright © 2016년 Keukey. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
